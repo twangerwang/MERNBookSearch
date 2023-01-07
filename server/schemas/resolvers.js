e49e79d34e2e2af7d9a6124bc2a71e76cd99b301
twangerwang <108110493+twangerwang@users.noreply.github.com>
2023-01-07 16:28:48 -0600

    refactored controller file to resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,68 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user.id },
+          { $push: { savedBooks: args.bookToSave } },
+          { new: true }
+        );
+        return user;
+      } else {
+        throw new AuthenticationError("Not logged in");
+      }
+    },
+    deleteBook: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user.id },
+          { $pull: { savedBooks: { bookId: args.bookToSave } } },
+          { new: true }
+        );
+        return user;
+      } else {
+        throw new AuthenticationError("Not logged in");
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
