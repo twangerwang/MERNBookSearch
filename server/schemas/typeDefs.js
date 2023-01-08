fb3af6b24dd38b2c8fb7386510ef185f60d29915
twangerwang <108110493+twangerwang@users.noreply.github.com>
2023-01-07 21:28:58 -0600

    added queries, mutations and updated app.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID!
+    authors: [string]
+    description: [string!]
+    bookId: [string!]
+    image: [string]
+    link: [string]
+    title: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    authors: [String!]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login: (email:String! password:String!):Auth
+    addUser: (username:String! email:String)! password:String!):Auth
+    saveBook: (bookToSave: BookInput!): User
+    deleteBook: (bookToDelete: ID!): User
+  }
+`;
+module.exports = typeDefs;
