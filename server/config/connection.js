06f0b7c13c89ad93eea3b89a41ed9ff529eba0c6
twangerwang <108110493+twangerwang@users.noreply.github.com>
2023-01-10 23:36:33 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/MERNBookSearch",
+  process.env.MONGODB_URI ||
+    "mongodb://localhost:127.0.0.1:27017/MERNBookSearch",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
